cb947b1b6bd3b405def734f39cb66281f892d31e
Aastha Bhatla <bhatlaaastha434@gmail.com>
2025-07-08 19:14:31 +0530

    Add PostgreSQL connection setup

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-// server.js
 const express = require('express');
+const pool = require('./db'); 
+require('dotenv').config();
+
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
+});
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('DB Error');
+  }
 });
 
 app.listen(PORT, () => {
