8f4b0f64ca366d73737064038d1f2bc2035812ff
Aastha Bhatla <bhatlaaastha434@gmail.com>
2025-10-03 15:45:20 +0530

    changes

--- routes/termsRoutes.js
+++ routes/termsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const termsController = require('../controllers/termsController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/', termsController.create);       
-router.get('/:id', termsController.getById);      
-router.post('/byTaxonomyIds', termsController.getByTaxonomyIds); 
-router.post('/', termsController.updateByIds);     
+router.post('/', authMiddleware, termsController.create);
+router.post('/', authMiddleware, termsController.updateByIds);
+router.get('/:id', termsController.getById);
+router.post('/byTaxonomyIds', termsController.getByTaxonomyIds);
 
 module.exports = router;
