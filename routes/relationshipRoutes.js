ef61a4c5a33aae6e4a7a4ad92e535006d9c31033
Aastha Bhatla <bhatlaaastha434@gmail.com>
2025-10-06 21:48:23 +0530

    changes

--- routes/relationshipRoutes.js
+++ routes/relationshipRoutes.js
@@ -5,10 +5,11 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/', relationshipController.createRelationship);
 router.get('/', relationshipController.getItems);
-router.post('/:id', relationshipController.updateRelationship);
-router.delete('/:id', relationshipController.deleteRelationship);
+
 router.post('/type-ids', authMiddleware, relationshipController.getTypeIds);
 
+router.post('/:id', relationshipController.updateRelationship);
+router.delete('/:id', relationshipController.deleteRelationship);
 
 module.exports = router;
 
