79a2f733c7f260dc8ee20d5835978b2dc2fcef5c
Aastha Bhatla <bhatlaaastha434@gmail.com>
2025-11-17 16:20:04 +0530

    changes

--- migrate.sh
+++ migrate.sh
@@ -13,5 +13,7 @@ docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/users.sql
 docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/taxonomy.sql
 docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/posts.sql
 docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/reviews.sql
+docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/wallet.sql
+docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/workspace.sql
 
 echo "DB Migrations complete"
