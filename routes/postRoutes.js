0bf20a6001e23d2ca99479943bb88f1d248e1732
Aastha Bhatla <bhatlaaastha434@gmail.com>
2025-11-22 03:56:37 +0530

    changes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const postController = require("../controllers/postController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
+router.get("/", postController.getPosts);
+router.post("/filter", postController.getPosts);
+
 router.use(authMiddleware);
 
 router.post("/by-terms", postController.getPostsByTerms);
-router.get("/", postController.getPosts);
-router.post("/filter", postController.getPosts);
 router.get("/slug/:slug", postController.getPostBySlug);
 router.get("/:id", postController.getPostById); 
 router.post("/", postController.createPost);
