9e43de65ab1c038e3e85367f93229be63c75b240
Mridul Goswami <mgoswami2189@gmail.com>
2026-01-29 15:01:26 +0530

    Removing a depricated sql file

--- db/schema/migrations/2026-01-22_wallet_v2_cleanup.sql
+++
@@ -1,6 +0,0 @@
--- 1) Remove legacy tables
-DROP TABLE IF EXISTS credit_transactions CASCADE;
-DROP TABLE IF EXISTS wallet CASCADE;
-
--- 2) Remove legacy user balance
-ALTER TABLE users DROP COLUMN IF EXISTS wallet_credits;
