0dded20305f73f90a2dece6a891517ba33696423
Mridul Goswami <mgoswami2189@gmail.com>
2026-02-04 15:33:05 +0530

    database updated

--- migrate.sh
+++ migrate.sh
@@ -5,20 +5,23 @@ cd "$(dirname "$0")"  # go to the script's directory
 
 echo "Running DB migrations..."
 
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/requests.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/users.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/taxonomy.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/posts.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/reviews.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/workspace.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/products.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/order.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/order_items.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/wallet.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/sessions.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/notifications.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/options.sql
-docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/jobs.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/requests.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/users.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/taxonomy.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/posts.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/reviews.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/workspace.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/products.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/order.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/order_items.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/wallet.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/sessions.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/notifications.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/options.sql
+# docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/jobs.sql
+docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/volunteer_applications.sql
+docker exec -i postgres-db psql -U mridul -d kaptaanAPI < db/schema/ngo_help_requests.sql
+
 
 
 echo "DB Migrations complete"
